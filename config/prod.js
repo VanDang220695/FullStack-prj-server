a386fea26e75d8d8568965e0aea286afe8544e37
Dang <nguyenvandang22061995@gmail.com>
2020-04-20 11:24:04 +0700

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,9 @@
 module.exports = {
-  googleClientId: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleClientId:
+    '123462597722-r78ep3e4rupura5757u01gnicfll25l1.apps.googleusercontent.com',
+  googleClientSecret: '38l81VyAO_QiKmdSQhvnX27N',
   mongoUsername: 'admin',
   mongoPassword: 'Kwm74JEl2yfE8E6W',
-  cookieKey: process.env.COOKIE_KEY,
+  cookieKey:
+    'sdadweqegfgsads12232@@@@1213fsdaasf^%6232sadgfg2adsbhghtweradjgeer',
 };
