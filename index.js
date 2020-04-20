1fedcc8f1f231d39425451cfa7e7edb0dc5732b4
Dang <nguyenvandang22061995@gmail.com>
2020-04-20 14:56:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,9 +31,8 @@ mongoose
   })
   .then(() => {
     console.log('Connect DB successful');
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running on Port ${PORT}`);
+    });
   })
   .catch((error) => console.log(error));
-
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is running on Port ${PORT}`);
-});
